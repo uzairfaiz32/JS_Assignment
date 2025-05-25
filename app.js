8160b15206e5e4e3e4c724a649b0890fdfa6fbe4
uzair <uazirfaiz@gmail.com>
2025-05-25 14:42:15 +0500

    updated

---
+++ app.js
@@ -0,0 +1,75 @@
+// // Assignment 1
+
+alert ("To Check whether user insert a value greater than 100 or less");
+
+let userNum = prompt("Enter your Number");
+if (userNum> 100){
+    alert("Number is greater than 100 and your entered value is " + userNum)
+}
+else{
+    alert("Number is less than 100 and your entered value is " + userNum)
+}
+// // Assignment 2
+alert ("To compare user insert values, which is greater");
+let userNum1 = prompt("Enter 1st value");
+let userNum2 = prompt("Enter 2nd value");
+if(userNum1>userNum2){
+    alert("User 1st value is greater than 2nd value and value is " + userNum1)
+
+}
+else{
+    alert("User 2nd value is greater than 1st value and value is " + userNum2)
+
+}
+// Assignment3
+alert("now we will see value is perfect divisible by 2 or not")
+var value1 = prompt("enter a value")
+if(value1%2 ===0){
+    alert("Value is perfect divisible by 2 ")
+}
+else{
+    alert("Value is not perfect divisible by 2 ")
+
+}
+
+// // Assignment4
+alert("now we will see value is perfect divisible by 3 or not")
+var value1 = prompt("enter a value")
+if(value1%3 ===0){
+    alert("Value is perfect divisible by 3 ")
+}
+else{
+    alert("Value is not perfect divisible by 3 ")
+
+}
+
+// Assignment5
+alert("now we will see value is perfect divisible by 5 or not")
+var value1 = prompt("enter a value")
+if(value1%5 ===0){
+    alert("Value is perfect divisible by 5 ")
+}
+else{
+    alert("Value is not perfect divisible by 5 ")
+
+}
+
+// // Assignment6
+alert("now we will see remainder after divisible by 2")
+var value1 = prompt("enter a value")
+var reminder = value1 %2;
+    alert("Reminder for given value is" + reminder)
+
+
+// // Assignment7
+alert("now we will see remainder after divisible by 3")
+var value1 = prompt("enter a value")
+var reminder = value1 %3;
+    alert("Reminder for given value is" + reminder)    
+
+    
+// Assignment8
+alert("now we will see remainder after divisible by 5")
+var value1 = prompt("enter a value")
+var reminder = value1 %5;
+    alert("Reminder for given value is" + reminder)    
